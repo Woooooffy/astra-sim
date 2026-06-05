d0fc664a88abcbbbdb8e51cb1a544e67af334e6e
Jacqueline Wang <jacquelineyujia@gmail.com>
2026-06-05 10:18:14 +0800

    updated scenario example

---
+++ scenarios/ring_16_750MB/run_ns3.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+set -x
+
+# find the absolute path to this script
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+ASTRA_SIM_DIR="${SCRIPT_DIR:?}/../.."
+
+# paths
+WORKLOAD="${SCRIPT_DIR:?}/workload/all_reduce"
+SYSTEM="${SCRIPT_DIR:?}/system.json"
+
+NS3_DIR="${ASTRA_SIM_DIR:?}"/extern/network_backend/ns-3
+
+
+NETWORK="${NS3_DIR:?}"/scratch/config/config_clos.txt
+LOGICAL_TOPOLOGY="${SCRIPT_DIR:?}"/ns3_logical_topo.json
+
+MEMORY="${PROJECT_DIR:?}/examples/remote_memory/analytical/no_memory_expansion.json"
+COMM_GROUP_CONFIGURATION="empty"
+
+cd "${NS3_DIR}/build/scratch"
+
+echo "Running simulation with WORKLOAD: ${WORKLOAD}"
+
+./ns3.42-AstraSimNetwork-default \
+    --workload-configuration=${WORKLOAD} \
+    --system-configuration=${SYSTEM} \
+    --network-configuration=${NETWORK} \
+    --remote-memory-configuration=${MEMORY} \
+    --logical-topology-configuration=${LOGICAL_TOPOLOGY} \
+    --comm-group-configuration=${COMM_GROUP_CONFIGURATION}
+
+cd "${SCRIPT_DIR:?}"
